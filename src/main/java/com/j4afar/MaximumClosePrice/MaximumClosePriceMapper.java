027ee545768f3ee7438a0cc98c62b38e8eaeff3b
Jaafar Amine <jaafar.amine@outlook.com>
2020-04-25 13:54:53 -0400

    Added counters for High and Low volume

--- src/main/java/com/j4afar/MaximumClosePrice/MaximumClosePriceMapper.java
+++ src/main/java/com/j4afar/MaximumClosePrice/MaximumClosePriceMapper.java
@@ -7,15 +7,28 @@ import org.apache.hadoop.mapreduce.Mapper;
 
 public class MaximumClosePriceMapper extends Mapper<LongWritable, Text, Text, FloatWritable>{
 	
+	// Used for counters
+	public enum Volume {
+		HIGH_VOLUME,
+		LOW_VOLUME
+	}
 	
 	@Override
 	public void map(LongWritable key, Text value, Context context)
 		throws IOException, InterruptedException {
 		String line = value.toString();
 		String[] items = line.split(",");
-		
+	
 		String stock = items[1];
 		Float closePrice = Float.parseFloat(items[6]);
+		
+		// Used for counters
+		int volume = Integer.parseInt(items[7]);
+		if (volume >= 500000)
+			context.getCounter(Volume.HIGH_VOLUME).increment(1);
+		else
+			context.getCounter(Volume.LOW_VOLUME).increment(1);
+		
 		
 		// output from our map job
 		context.write(new Text(stock), new FloatWritable(closePrice));
